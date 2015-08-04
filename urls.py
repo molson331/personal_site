bb3f93fe3d32ace5f16a56457dbbf97d093057f6
Melissa Olson <melissao@qti.qualcomm.com>
2015-08-04 11:18:23 -0700

    Start home page with lib imports, header, & footer

--- urls.py
+++ urls.py
@@ -3,10 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from personal.views import IndexView
 
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', IndexView.as_view(), name='index'),
 )
